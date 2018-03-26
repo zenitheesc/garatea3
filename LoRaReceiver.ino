801efafd0c8b4fa37c0821dd652ea572d0edbf08
cescoow <francescorossilena@gmail.com>
2018-03-26 18:38:52 -0300

    LoRa Receiver - Cód. base

---
+++ LoRaReceiver.ino
@@ -0,0 +1,39 @@
+#include <SPI.h>
+#include <LoRa.h>
+String stringdata = "";
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial);
+
+  Serial.println("LoRa Receiver");
+
+  Serial.println("LoRa Sender");
+  LoRa.setPins(7, A0, 2);
+  if (!LoRa.begin(433E6)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+  LoRa.setSignalBandwidth(125E3);
+  LoRa.setSpreadingFactor(11);
+}
+
+void loop() {
+  // try to parse packet
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) {
+    // received a packet
+    Serial.print("Received packet '");
+
+    // read packet
+    while (LoRa.available()) {
+      stringdata.concat((char)LoRa.read());
+    }
+
+    // print RSSI of packet
+    Serial.println(stringdata);
+    stringdata = "";
+    Serial.print("' with RSSI ");
+    Serial.println(LoRa.packetRssi());
+  }
+}
